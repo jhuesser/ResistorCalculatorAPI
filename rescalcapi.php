e3d1dcbd8184f1e6f43d4f5c32abef18c7e75448
Jonas Hüsser <jonas.huesser+github@gmail.com>
2016-09-22 20:18:42 +0200

    Created the php file

---
+++ rescalcapi.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Resistor Calculator API</title>
+	
+	
+	</head>
+	<body>
+		<?php
+			
+			//Initalize values
+			$firstcolor=$_POST['firstcolor'];
+			$secondcolor=$_POST['secondcolor'];
+			$thirdcolor=$_POST['thirdcolor'];
+			$fourthcolor=$_POST['fourthcolor'];
+			$hasFiveRings=$_POST['hasFiveRings];
+			$fithcolor=$_POST['fithcolor'];
+			
+			
+		?>
+	</body>
+</html>
