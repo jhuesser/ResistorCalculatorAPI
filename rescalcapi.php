ad3e02b9b8a760f99c26e0f45c8d148e5d007280
Jonas Hüsser <jonas.huesser+github@gmail.com>
2016-09-27 19:32:55 +0200

    Commented the code / set expire date to now

--- rescalcapi.php
+++ rescalcapi.php
@@ -15,6 +15,7 @@
 				
 				}
 			$resultInText=$_GET['resultInText'];
+			$DATE = date(DATE_RFC822);
 			
 			
 			//check if error occured
@@ -22,13 +23,14 @@
 			function checkIfError($hasError, $errorNmb, $errorMsg){
 				
 				if ($hasError == 1){
+					//Set error message
 					exit("Error: " . $errorNmb . " " . $errorMsg);
 					
 					}
 				}
 			
 			function setFirstColorValue($color){
-				
+				//checks the first color and assign the value
 				switch($color){
 					case "brown":
 						$colorvalue=1;
@@ -58,6 +60,7 @@
 						$colorvalue=9;
 						break;
 					default:
+					//on error
 						$errorMsg = "The color " . $color . " can't be a color at this position";
 						$errorNmb = "101";
 						$hasError = 1;
@@ -70,14 +73,14 @@
 			
 			
 			function setSecondColorValue($color) {
-				
+				//second color values are the same as first + a black option
 				if ($color == "black") {
 						$colorvalue=0;
 					} else {
 						$colorvalue = setFirstColorValue($color);
 						
 						}
-				
+				//on error
 				checkIfError($GLOBALS['hasError'], $GLOBALS['errorNmb'], $GLOBALS['errorMsg']);
 				return $colorvalue;
 				
@@ -85,6 +88,7 @@
 				
 				
 			function calculateMultipiler($color) {
+				//Multipiler is 3rd or 4th ring, values are the same
 				switch($color){
 							
 							case "silver":
@@ -124,6 +128,7 @@
 								$colorvalue=1000000000;
 								break;
 							default:
+							//on error
 								$errorMsg = "The color " . $color . " can't be a color at this position";
 								$errorNmb = "101";
 								$hasError = 1;
@@ -138,16 +143,17 @@
 				
 			
 			function setThirdColorValue($color, $hasFiveRings) {
-				
+				//sets value for 3rd ring
 				if ($hasFiveRings == 1){
-					
+					//if 5 rings == true, use same values as in second ring
 					$colorvalue = setSecondColorValue($color);
 					
 				} elseif ($hasFiveRings == 0) {
-						
+						//else calculate the multipiler
 					$colorvalue = calculateMultipiler($color);
 						
 				} else {
+					//on error
 					$errorMsg = "The result could not be generated. Maybe your resultInText request is corrupt.";
 					$errorNmb = "201";
 					$hasError = 1;
@@ -162,13 +168,13 @@
 				
 			
 			function setFourthColorValue($color, $hasFiveRings) {
-				
+				//if 5 rings == true this is the multipiler
 				if ($hasFiveRings == 1){
 					
 					$colorvalue = calculateMultipiler($color);
 					
 			} elseif ($hasFiveRings == 0) {
-						
+					//else it's the tolerance
 					switch($color){
 						
 						case "no-color":
@@ -187,6 +193,7 @@
 							$colorvalue=0;
 					}
 				} else{
+					//on error
 					$errorMsg = "The resultå could not be generated. Maybe your resultInText request is corrupt.";
 					$errorNmb = "201";
 					$hasError = 1;
@@ -201,7 +208,7 @@
 				
 				
 				function setFifthColorValue($color){
-					
+					//tolerance of resistor with 5 rings
 					switch($color) {
 						
 						case "brown":
@@ -227,20 +234,26 @@
 					}
 				
 			function caclulateResult($firstvalue, $secondvalue, $thirdvalue, $fourthvalue, $fifthvalue, $hasFiveRings, $resultInText){
-				
+				//calculates the result and formats the output.
 				if ($hasFiveRings == 1 ){
-					
+					//for a 5 ring resistor
+					//put the number together
 					$numberToMultiply = $firstvalue . $secondvalue . $thirdvalue;
+					//multiply that number with the multipiler, this is the ohm value
 					$ohm = $numberToMultiply * $fourthvalue;
+					//tolerance is the 5th ring
 					$tolerance = $fifthvalue;
 					
 					if ($resultInText == 1 ){
+						//if user wants the result as text, set up the text
 						$RESULT="Your resistor has " . $ohm . " Ohm. The tolerance is " . $tolerance . ".";
 						
 						} elseif ($resultInText == 0) {
+							//if user wants json, put a string with : as delmiter togheter.
 							$RESULT=$ohm . ":" . $tolerance;
 							
 							} else {
+								//on error
 								$errorMsg = "The result could not be generated. Maybe your resultInText request is corrupt.";
 								$errorNmb = "201";
 								$hasError = 1;
@@ -249,18 +262,25 @@
 								}
 					
 					} elseif ($hasFiveRings == 0){
+						//for resistor with 4 rings
+						//put the number together
 						$numberToMultiply = $firstvalue . $secondvalue;
+						//multiply that number with the multipiler, this is the ohm value
 						$ohm = $numberToMultiply * $thirdvalue;
+						//tolerance is the 4th ring
 						$tolerance = $fourthvalue;
 						
 						
 						if ($resultInText == 1 ){
+							//if user wants the result as text, set up the text
 						$RESULT="Your resistor has " . $ohm . " Ohm. The tolerance is " . $tolerance . ".";
 						
 						} elseif ($resultInText == 0) {
+							//if user wants json, put a string with : as delmiter togheter.
 							$RESULT=$ohm . ":" . $tolerance;
 							
 							} else {
+								//on error
 								$errorMsg = "The result could not be generated. Maybe your resultInText request is corrupt.";
 								$errorNmb = "201";
 								$hasError = 1;
@@ -269,6 +289,7 @@
 								}
 						
 						} else {
+							//on error
 							$errorMsg = "The result could not be calculated. Maybe your hasFiveRings request is corrupt.";
 							$errorNmb = "202";
 							$hasError = 1;
@@ -283,10 +304,23 @@
 				return $RESULT;
 				
 				}
+/*
+
+	THE SCRIPT STARTS HERE
+	HERE TO FUNCTIONS ABOVE ARE GETTING CALLED
+
+
+
+
+*/				
+
+				//this block is needed to avoid errors
 				$hasError=0;
 				$errorNmb=0;
 				$errorMsg=0;
 				
+				
+				//calling the functions to get the values of the colors
 				$firstvalue=setFirstColorValue($firstcolor);
 				
 				$secondvalue=setSecondColorValue($secondcolor);
@@ -299,34 +333,42 @@
 					$fifthvalue=setFifthColorValue($fifthcolor);
 					
 				} elseif ($hasFiveRings == 0){
+					//to avoid calculating error
 					$fithvalue=0;
 					
 					} else {
+						//on error
 						$errorMsg = "The result could not be calculated. Maybe your hasFiveRings request is corrupt.";
 						$errorNmb = "202";
 						$hasError = 1;
 						$colorvalue=0;	
 						
 						}
+						
 				checkIfError($GLOBALS['hasError'], $GLOBALS['errorNmb'], $GLOBALS['errorMsg']);
+				//calculate the result
 				$RESULT = caclulateResult($firstvalue, $secondvalue, $thirdvalue, $fourthvalue, $fifthvalue, $hasFiveRings, $resultInText);
 				checkIfError($GLOBALS['hasError'], $GLOBALS['errorNmb'], $GLOBALS['errorMsg']);
 				
+				
+				//echo the result
 				if ($resultInText == 0){
-				
+				//if user wants json
+				//cut the result at delmiter ":"
 				$cutResult = explode(":", $RESULT);
-				
+				//write the json array
 				$result_json = array('ohm' => $cutResult[0], 'tolerance' => $cutResult[1]);
-				
+				//write the headers
 				header('Cache-Contoil: no-cache, must-revalidate');
-				header('Expires: Mon 27 Jul 1997 05:00:00 GMT');
+				header('Expires: ' . $DATE);
+				
 				
 				header('Content-type: application/json');
-				
+				//echo json
 				echo json_encode($result_json);
 				
 				} else {
-					
+					//echo text
 					echo $RESULT;
 					
 					}
